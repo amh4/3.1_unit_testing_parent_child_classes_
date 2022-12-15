fd109ce09518e884a5015e679caa756d0c803964
Adam Hoar <42139484+amh4@users.noreply.github.com>
2022-12-15 16:27:18 +0000

    Added tests

--- spec/music_integration_spec.rb
+++ spec/music_integration_spec.rb
@@ -14,9 +14,9 @@ RSpec.describe "integration" do
   it "searches track by keyword" do
     track_1 = Track.new("One two three", "four five six")
     track_2 = Track.new("Seven eight nine", "ten elevel twelve")
-   library = MusicLibrary.new
-   library.add(track_1)
-   library.add(track_2)
+    library = MusicLibrary.new
+    library.add(track_1)
+    library.add(track_2)
     expect(library.search("two")).to eq [track_1]
   end
 end
